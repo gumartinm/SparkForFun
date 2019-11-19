111be79b263e4ecd4d6b81477865f6db23f3629d
Gustavo Martin Morcuende <gu.martinm@gmail.com>
2019-11-19 23:55:14 +0100

    ExampleApp

---
+++ playground/playground-spark-hive/src/main/scala/de/example/playground/spark/kafka/ExampleApp.scala
@@ -0,0 +1,8 @@
+// Author: Gustavo Martin Morcuende
+package de.example.playground.spark.kafka
+
+import com.typesafe.scalalogging.LazyLogging
+
+object ExampleApp extends App with LazyLogging {
+  logger.info("THIS IS JUST AN EXAMPLE")
+}
