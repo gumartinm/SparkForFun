82cb1e0aca6f748ef813945a62b5cedb904f8eaf
Gustavo Martin Morcuende <gu.martinm@gmail.com>
2020-07-13 03:41:33 +0200

    AwesomeJob

--- pyspark/example/awesome_job.py
+++ pyspark/example/awesome_job.py
@@ -2,7 +2,7 @@ class AwesomeJob:
 
     def __init__(self, source_path, destination_path, spark_session, awesome_service):
         self.source_path = source_path
-        self.destionation_path = destination_path
+        self.destination_path = destination_path
         self.spark_session = spark_session
         self.awesome_service = awesome_service
 
@@ -11,4 +11,4 @@ class AwesomeJob:
         json_schema = json_data_frame.schema
         upper_case_json_schema = self.awesome_service.rename_columns_to_upper_case(json_schema)
         upper_case_json_data_frame = self.spark_session.createDataFrame(json_data_frame.rdd, upper_case_json_schema)
-        upper_case_json_data_frame.write.json(self.destionation_path)
+        upper_case_json_data_frame.write.json(self.destination_path)
