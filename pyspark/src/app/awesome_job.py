621ba6a70477aca9a3c2e117bf54680ca0f01f3e
Gustavo Martin Morcuende <gu.martinm@gmail.com>
2020-08-26 02:32:53 +0200

    AwesomeJob

--- pyspark/src/app/awesome_job.py
+++ pyspark/src/app/awesome_job.py
@@ -1,24 +1,46 @@
-class AwesomeJob:
+from pyspark.sql.types import StringType, StructField, StructType
 
-    def __init__(self, source_path, destination_path, spark, awesome_service):
+
+class AwesomeJob:
+    __DATABASE = 'testing'
+    __TABLE = 'example'
+
+    def __init__(self, source_path, destination_path, spark_session, awesome_service):
         self.source_path = source_path
         self.destination_path = destination_path
-        self.spark = spark
+        self.spark_session = spark_session
         self.awesome_service = awesome_service
-        self.logger = AwesomeJob.__logger(spark)
+        self.logger = AwesomeJob.__logger(spark_session)
 
     def run(self):
         self.logger.info("Running AwesomeJob")
 
-        json_data_frame = self.spark.read.json(self.source_path)
-        json_schema = json_data_frame.schema
-        upper_case_json_schema = self.awesome_service.rename_columns_to_upper_case(json_schema)
-        upper_case_json_data_frame = self.spark.createDataFrame(json_data_frame.rdd, upper_case_json_schema)
-        upper_case_json_data_frame.write.json(self.destination_path)
+        json_schema = StructType(
+            [
+                StructField("name", StringType()),
+                StructField("surname", StringType())
+            ]
+        )
+        data_frame = self.spark_session.read.schema(json_schema).json(path=self.source_path)
+        schema = data_frame.schema
+
+        upper_case_schema = self.awesome_service.rename_columns_to_upper_case(schema)
+        upper_case_data_frame = self.spark_session.createDataFrame(data_frame.rdd, upper_case_schema)
+
+        self.spark_session.sql(f'CREATE DATABASE IF NOT EXISTS {AwesomeJob.__DATABASE}')
+        self.spark_session.sql(
+            f'CREATE TABLE IF NOT EXISTS `{AwesomeJob.__DATABASE}`.`{AwesomeJob.__TABLE}` (`NAME` STRING, `SURNAME` STRING) '
+            f'USING PARQUET '
+            f'OPTIONS ( '
+            f'path \'{self.destination_path}\' '
+            f')')
+        upper_case_data_frame.write \
+            .mode("overwrite") \
+            .insertInto(f'{AwesomeJob.__DATABASE}.{AwesomeJob.__TABLE}')
 
         self.logger.info("End running AwesomeJob")
 
     @staticmethod
-    def __logger(spark):
-        log4j_logger = spark.sparkContext._jvm.org.apache.log4j
+    def __logger(spark_session):
+        log4j_logger = spark_session.sparkContext._jvm.org.apache.log4j
         return log4j_logger.LogManager.getLogger(__name__)
