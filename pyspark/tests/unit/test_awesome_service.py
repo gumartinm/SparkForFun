3e6d8d56a456aacf3609bdcfc119f046ca431abd
Gustavo Martin Morcuende <gu.martinm@gmail.com>
2020-08-26 02:35:11 +0200

    test_awesome_service without unittest

--- pyspark/tests/unit/test_awesome_service.py
+++ pyspark/tests/unit/test_awesome_service.py
@@ -1,52 +1,48 @@
-import unittest
+from app.awesome_service import AwesomeService
 
 from pyspark.sql.types import StructType, StringType, StructField, ArrayType
 
-from app.awesome_service import AwesomeService
 
-
-class AwesomeServiceTest(unittest.TestCase):
-
-    def test_rename_columns_to_upper_case(self):
-        service = AwesomeService()
-        some_shema = StructType(
-            [
-                StructField(
-                    "Level1ColumnA",
-                    StructType(
-                        [
-                            StructField(
-                                "Level2ColumnA",
-                                StructType(
-                                    [StructField("Level3ColumnA", StringType())]
-                                )
+def test_rename_columns_to_upper_case(self):
+    service = AwesomeService()
+    some_shema = StructType(
+        [
+            StructField(
+                "Level1ColumnA",
+                StructType(
+                    [
+                        StructField(
+                            "Level2ColumnA",
+                            StructType(
+                                [StructField("Level3ColumnA", StringType())]
                             )
-                        ]
-                    )
-                ),
-                StructField("Level1ColumnB", ArrayType(StringType()))
-            ]
-        )
+                        )
+                    ]
+                )
+            ),
+            StructField("Level1ColumnB", ArrayType(StringType()))
+        ]
+    )
 
-        expected_schema = StructType(
-            [
-                StructField(
-                    "LEVEL1COLUMNA",
-                    StructType(
-                        [
-                            StructField(
-                                "LEVEL2COLUMNA",
-                                StructType(
-                                    [StructField("LEVEL3COLUMNA", StringType())]
-                                )
+    expected_schema = StructType(
+        [
+            StructField(
+                "LEVEL1COLUMNA",
+                StructType(
+                    [
+                        StructField(
+                            "LEVEL2COLUMNA",
+                            StructType(
+                                [StructField("LEVEL3COLUMNA", StringType())]
                             )
-                        ]
-                    )
-                ),
-                StructField("LEVEL1COLUMNB", ArrayType(StringType()))
-            ]
-        )
+                        )
+                    ]
+                )
+            ),
+            StructField("LEVEL1COLUMNB", ArrayType(StringType()))
+        ]
+    )
 
-        result_schema = service.rename_columns_to_upper_case(some_shema)
+    result_schema = service.rename_columns_to_upper_case(some_shema)
 
-        self.assertEqual(expected_schema, result_schema)
+    assert expected_schema == result_schema
