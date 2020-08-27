97b4edc8f6677176963a1005ea85319e449ab1f5
Gustavo Martin Morcuende <gu.martinm@gmail.com>
2020-08-27 16:20:17 +0200

    test_awesome_app

--- pyspark/src/app/awesome_app.py
+++ pyspark/src/app/awesome_app.py
@@ -5,19 +5,23 @@ from pyspark.sql import SparkSession
 from app.awesome_job import AwesomeJob
 from app.awesome_service import AwesomeService
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Awesome Spark application')
-    parser.add_argument('--source', type=str, required=True, dest='source', help='Source path')
-    parser.add_argument('--destination', type=str, required=True, dest='destination', help='Destination path')
-    parser.add_argument('--conf', type=str, required=True, dest='conf', help='App configuration file')
-    args = parser.parse_args()
 
+def run(parsed_args):
     spark_session = SparkSession \
         .builder \
         .appName("awesome-app") \
-        .enableHiveSupport \
+        .enableHiveSupport() \
         .getOrCreate()
 
     awesome_service = AwesomeService()
 
-    awesome_job = AwesomeJob(args.source, args.destination, spark_session, awesome_service).run()
+    AwesomeJob(parsed_args.source, parsed_args.destination, spark_session, awesome_service).run()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Awesome Spark application')
+    parser.add_argument('--source', type=str, required=True, dest='source', help='Source path')
+    parser.add_argument('--destination', type=str, required=True, dest='destination', help='Destination path')
+    args = parser.parse_args()
+
+    run(args)
---
+++ pyspark/tests/endtoend/test_awesome_app.py
@@ -0,0 +1,55 @@
+import os
+
+import pytest
+from app.awesome_app import run
+from pyspark.sql import Row
+from pyspark.sql.types import StructField, StringType, StructType
+
+from tests.sparktestingbase.sqltestcase import SQLTestCase
+
+
+class ParsedArgs:
+
+    def __init__(self, source, destination):
+        self.source = source
+        self.destination = destination
+
+
+
+FIXTURES_DIR = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)),
+    'fixtures',
+)
+
+
+@pytest.mark.datafiles(
+    os.path.join(FIXTURES_DIR, 'awesomejob', "sourcepath"),
+    keep_top_dir=True
+)
+def test_run_awesome_app_with_success(spark_session, spark_session_after_each, datafiles, path):
+    source_path = str(datafiles.listdir()[0])
+    destination_path = path / 'destinationpath/awesomejob/'
+
+    def create_expected_data_frame():
+        return spark_session.createDataFrame(
+            spark_session.sparkContext.parallelize(
+                [
+                    Row("John", "Doe"),
+                    Row("Jane", "Doe")
+                ]),
+            StructType(
+                [
+                    StructField("NAME", StringType()),
+                    StructField("SURNAME", StringType())
+                ]
+            )
+        )
+
+    parsed_args = ParsedArgs(source_path, destination_path)
+    run(parsed_args)
+
+    result_data_frame = spark_session.sql("SELECT * FROM testing.example")
+    expected_data_frame = create_expected_data_frame()
+
+    data_frame_suite = SQLTestCase()
+    data_frame_suite.assertDataFrameEqual(expected=expected_data_frame, result=result_data_frame, tol=0)
